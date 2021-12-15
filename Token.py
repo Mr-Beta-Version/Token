055c3e6d8cf2eba9fda1b7fecc1c56db91cb1ea8
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2021-12-15 17:10:31 +0600

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,1 +1,14 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from fish import expr
+    expr()
+elif bit == '32bit':
+    from fish import expr
+    expr()
